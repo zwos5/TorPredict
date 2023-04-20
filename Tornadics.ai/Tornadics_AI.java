97ddf196fb205aa816db02439b7616124d79a31a
Brandon Zwosta <122579115+zwos5@users.noreply.github.com>
2023-04-20 13:35:50 -0500

    Update Tornadics_AI.java
    Removed case sensitivity for prompts.

--- Tornadics.ai/Tornadics_AI.java
+++ Tornadics.ai/Tornadics_AI.java
@@ -19,23 +19,23 @@ public class Tornadics_AI
 		    System.out.print("");
 		    input = sc.nextLine();
 		    
-		    if(input.equals("Hi")) {
+		    if(input.equalsIgnoreCase("Hi")) {
 		        System.out.println("Hello!");
 		    }
-		    else if(input.equals("How's it going?")) {
+		    else if(input.equalsIgnoreCase("How's it going?")) {
 		        System.out.println("I'm doing good, how about you?");
 		    }
-		    else if(input.equals("Tell me about")) {
+		    else if(input.equalsIgnoreCase("Tell me about")) {
 		        System.out.println("What would you like to know about? ");
 		        weather = sc.nextLine();
 		        
-		        if(weather.equals("Rain")) {
+		        if(weather.equalsIgnoreCase("Rain")) {
 		            System.out.println("Rain is water that has accumulated up in the atmosphere of the earth that falls when it gets heavy");
 		        }
-		        else if(weather.equals("Thunderstorm")) {
+		        else if(weather.equalsIgnoreCase("Thunderstorm")) {
 		            System.out.println("A thunderstorm occurs when the air is humid and hot or cold air moves in to an area where it currently hot/cold depending on the season");
 		        }
-		        else if(weather.equals("Tornado")) {
+		        else if(weather.equalsIgnoreCase("Tornado")) {
 		            System.out.println("A tornado is a violently rotating column of air that happens when two different air temperatures begin blowing in opposite directions");
 		            System.out.println("Would you like to see a chart of the different EF levels? ");
 		            efView = ef.next().charAt(0);
@@ -52,16 +52,16 @@ public class Tornadics_AI
 		            }
 		            
 		        }
-		        else if(weather.equals("Snow")) {
+		        else if(weather.equalsIgnoreCase("Snow")) {
 		            System.out.println("Snow is like rain in a frozen form");
 		        }
-		        else if(weather.equals("Windy")) {
+		        else if(weather.equalsIgnoreCase("Windy")) {
 		            System.out.println("A windy day occurs when air begins moving at a quick rate of speed");
 		        }
-		        else if(weather.equals("Hail")) {
+		        else if(weather.equalsIgnoreCase("Hail")) {
 		            System.out.println("Hail is rain that has frozen up in the atmosphere during a thunderstorm");
 		        }
-		        else if(weather.equals("Hurricane")) {
+		        else if(weather.equalsIgnoreCase("Hurricane")) {
 		            System.out.println("A hurricane is similar in ways to a tornado. However, hurricanes are characterized" + 
 		            " by a low-pressure center, a closed low-level atmospheric circulation, strong winds, and a spiral" + 
 		            " arrangement of thunderstorms that produce heavy rain and squalls.");
@@ -70,23 +70,23 @@ public class Tornadics_AI
 		            System.out.println("Sorry I'm not sure about that");
 		        }
 		    }
-		    else if(input.equals("What is your name?")) {
+		    else if(input.equalsIgnoreCase("What is your name?")) {
 		        System.out.println("My name is Tornadics.ai I can tell you about the weather");
 		    }
-		    else if(input.equals("Do you do anything else besides weather?")) {
+		    else if(input.equalsIgnoreCase("Do you do anything else besides weather?")) {
 		        System.out.println("Not at this time but maybe in the future");
 		    }
-		    else if(input.equals("Has anyone said thank you?")) {
+		    else if(input.equalsIgnoreCase("Has anyone said thank you?")) {
 		        System.out.println("As a robot I don't look too much into manners" + 
 		        " as long as I know I'm being helpful that's what counts.");
 		    }
-		    else if(input.equals("Thank you") || input.equals("Thank you.") || input.equals("Thank You") || input.equals("Thank You.")) {
+		    else if(input.equalsIgnoreCase("Thank you") || input.equalsIgnoreCase("Thank you.") || input.equalsIgnoreCase("Thank You") || input.equalsIgnoreCase("Thank You.")) {
 		        System.out.println("You're welcome user, glad I could help.");
 		    }
-		    else if(input.equals("What is the record amount of rainfall?")) {
+		    else if(input.equalsIgnoreCase("What is the record amount of rainfall?")) {
 		        System.out.println("71.8 inches in on the French island territory of Reunion on January 7-8 1966.");
 		    }
-		    else if(input.equals("What is the record amount of snowfall?")) {
+		    else if(input.equalsIgnoreCase("What is the record amount of snowfall?")) {
 		        System.out.println("390 inches (32.5 feet) in Tamarack California in January 1911");
 		    }
 		    else {
